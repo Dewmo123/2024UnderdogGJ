5add27f370592f01bf6234758248d8df808c09af
Dewmo123 <akh200808@gmail.com>
2024-12-18 21:43:23 +0900

    .fix Upper

--- Assets/00.Work/SHS/01.Scripts/Player/RotateablePlayerVIsual.cs
+++ Assets/00.Work/SHS/01.Scripts/Player/RotateablePlayerVIsual.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateablePlayerVIsual : MonoBehaviour
+public class RotateablePlayerVIsual : MonoBehaviour, IPlayerComponent
 {
+    private Player _player;
     [SerializeField] private Transform upperBody;
     [SerializeField] private Transform upperBodyVisual;
     [SerializeField] float minAngle = 45f;
     [SerializeField] float maxAngle = 45f;
     [SerializeField] Vector2 offset;
+
+    public void Initialize(Player player)
+    {
+        _player = player;
+        gameObject.SetActive(false);
+        
+    }
+    
     public void Rotate(float angle)
     {
         angle = Mathf.Clamp(angle, -maxAngle, minAngle);
+        upperBody.localRotation = Quaternion.Euler(0, 0, angle);
+
+        upperBody.localPosition = offset;
+        upperBodyVisual.localPosition = -offset;
+    }
+    public void ReverseRotate(float angle)
+    {
+        angle = Mathf.Clamp(angle, -(180-maxAngle), 180-minAngle);
         upperBody.localRotation = Quaternion.Euler(0, 0, angle);
 
         upperBody.localPosition = offset;
