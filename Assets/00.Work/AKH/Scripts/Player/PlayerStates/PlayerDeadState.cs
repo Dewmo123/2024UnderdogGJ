6a48582eff195ef22beb07b14913866075ea6248
Dewmo123 <akh200808@gmail.com>
2024-12-23 09:40:45 +0900

    .fix aimDead

--- Assets/00.Work/AKH/Scripts/Player/PlayerStates/PlayerDeadState.cs
+++ Assets/00.Work/AKH/Scripts/Player/PlayerStates/PlayerDeadState.cs
@@ -7,6 +7,21 @@ public class PlayerDeadState : PlayerState
     public PlayerDeadState(PlayerStateMachine stateMachine, string animName, Player player) : base(stateMachine, animName, player)
     {
     }
+    private RotateablePlayerVIsual _visual;
+    private PlayerMovement _movement;
+    private PlayerAnimator _anim;
+    public override void Enter()
+    {
+        _movement = _player.GetCompo<PlayerMovement>();
+        _visual = _player.GetCompo<RotateablePlayerVIsual>();
+        _anim = _player.GetCompo<PlayerAnimator>();
+
+        _visual.gameObject.SetActive(false);
+        _anim.Renderer.enabled = true;
+        _anim.ChangeAnimator(null, true);
+        Time.timeScale = 1;
+        base.Enter();
+    }
     public override void UpdateState()
     {
         base.UpdateState();
